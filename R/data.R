29650f024aee6107f1186b47c874e074b4972dba
andreaskapou <kapouranis.andreas@gmail.com>
2016-03-29 10:00:18 +0100

    Local package datasets

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' Synthetic data for BPRMeth package
+#'
+#' A synthetic dataset containinig 600 entries.
+#'
+#' @format A list with 600 elements, where each element element is an L x 3
+#'  matrix of observations, where:
+#' \describe{
+#'   \item{1st column}{locations of obseravtions}
+#'   \item{2nd column}{total trials at corresponding locations}
+#'   \item{3rd column}{number of successes at corresponding locations}
+#' }
+"meth_data"
+
+#' Synthetic data for mpgex package
+#'
+#' Corresponding gene expression data for the 'meth_data'
+#'
+#' @format A vector of length 600
+"gex_data"
