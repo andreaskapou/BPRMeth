d7b1df1fbab20f16c56e312ecdb10a3a48a91a17
andreaskapou <kapouranis.andreas@gmail.com>
2016-03-29 09:53:14 +0100

    Read files containing chromosome sizes

---
+++ R/read_chrom_size.R
@@ -0,0 +1,23 @@
+#' Read file containing genome chromosome sizes.
+#'
+#' \code{read_chrom_size} reads a file containing genome chromosome sizes using
+#'  the \code{\link[data.table]{fread}} function.
+#'
+#' @param file The name of the file to read data values from.
+#'
+#' @return A \code{\link[data.table]{data.table}} object.
+#'
+#' @seealso \code{\link{read_rna_encode_caltech}},
+#'  \code{\link{read_bs_encode_haib}}
+#'
+#' @export
+read_chrom_size <- function(file){
+  message("Reading file ", file, " ...")
+  data_raw <- data.table::fread(input = file,
+                               sep = "\t",
+                               header = FALSE,
+                               col.names = c("chr", "size"))
+
+  message("Done!\n")
+  return(data_raw)
+}
