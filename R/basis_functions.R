f728b6c490d0fbfa6bc81ed66741deef8bc0303a
andreaskapou <kapouranis.andreas@gmail.com>
2016-05-13 13:38:35 +0100

    Change names of functions

--- R/basis_functions.R
+++ R/basis_functions.R
@@ -1,6 +1,6 @@
 #' Create a polynomial object
 #'
-#' \code{polynomial.object} creates objects of class "polynomial". These
+#' \code{create_polynomial_object} creates objects of class "polynomial". These
 #' objects can be used as input to complex functions in order to perform
 #' computations depending on the class of  basis function. E.g see
 #' \code{\link{design_matrix}} generic function.
@@ -9,13 +9,15 @@
 #'
 #' @return An object of class 'polynomial'
 #'
-#' @seealso \code{\link{design_matrix}}, \code{\link{rbf.object}}
+#' @author C.A.Kapourani \email{C.A.Kapourani@@ed.ac.uk}
+#'
+#' @seealso \code{\link{design_matrix}}, \code{\link{create_rbf_object}}
 #'
 #' @examples
-#' (obj <- polynomial.object(M = 2))
+#' (obj <- create_polynomial_object(M = 2))
 #'
 #' @export
-polynomial.object <- function(M = 1){
+create_polynomial_object <- function(M = 1){
   # Check that M is numberic and integer
   assertthat::assert_that(is.numeric(M))
   assertthat::assert_that(M %% 1 == 0)
@@ -27,28 +29,30 @@ polynomial.object <- function(M = 1){
 
 #' Create an RBF object
 #'
-#' \code{rbf.object} creates objects of class "rbf". These objects can be used
-#' as input to complex functions in order to perform computations depending on
-#' the basis function. E.g see \code{\link{design_matrix}} generic function.
+#' \code{create_rbf_object} creates objects of class "rbf". These objects can be
+#' used as input to complex functions in order to perform computations depending
+#' on the basis function. E.g see \code{\link{design_matrix}} generic function.
 #'
 #' @param M The number of the RBFs.
 #' @param gamma Inverse width of radial basis function.
 #' @param mus Optional centers of the RBF.
 #' @param eq_spaced_mus Logical, if TRUE, equally spaced centers are created,
-#'  otherwise centers are created using \code{\link[stats]{kmeans}} algorithm.
+#'   otherwise centers are created using \code{\link[stats]{kmeans}} algorithm.
 #' @param whole_region Logical, indicating if the centers will be evaluated
-#'  equally spaced on the whole region, or between the min and max of the
-#'  observation values.
+#'   equally spaced on the whole region, or between the min and max of the
+#'   observation values.
 #'
-#' @return An object of type 'RBF'.
+#' @return An object of class 'rbf'.
 #'
-#' @seealso \code{\link{design_matrix}}, \code{\link{polynomial.object}}
+#' @author C.A.Kapourani \email{C.A.Kapourani@@ed.ac.uk}
+#'
+#' @seealso \code{\link{design_matrix}}, \code{\link{create_polynomial_object}}
 #'
 #' @examples
-#' (obj <- rbf.object(M = 2))
+#' (obj <- create_rbf_object(M = 2))
 #'
 #' @export
-rbf.object <- function(M = 2, gamma = NULL, mus = NULL, eq_spaced_mus = TRUE,
+create_rbf_object <- function(M = 2, gamma = NULL, mus = NULL, eq_spaced_mus = TRUE,
                                                          whole_region = TRUE){
   assertthat::assert_that(is.numeric(M))
   assertthat::assert_that(is.logical(eq_spaced_mus))
@@ -87,47 +91,49 @@ rbf.object <- function(M = 2, gamma = NULL, mus = NULL, eq_spaced_mus = TRUE,
 #------------------------------------------------------------
 
 
-#' Apply polynomial basis function.
-#'
-#' Applies the polynomial basis function of degree M to the input X.
-#'
-#' @param X The input data, either a scalar, vector or matrix.
-#' @param M Integer, denoting the degree of the polynomial basis that will be
-#'  applied to the input X. M should not be negative.
-#'
-#' @return Input X, after being transformed from the polynomial basis function.
-#'
-#' @seealso \code{\link{polynomial.object}}, \code{\link{rbf_basis}}
-#'
-#' @examples
-#' out <- polynomial_basis(X = 3, M = 2)
-#' #
-#' out <- polynomial_basis(X = c(2, 3, 4), M = 2)
-#'
-#' @export
-polynomial_basis <- function(X, M = 1){
+# (Internal) Apply polynomial basis function.
+#
+# Apply the polynomial basis function of degree M to the input X.
+#
+# @param X The input data, either a scalar, vector or matrix.
+# @param M Integer, denoting the degree of the polynomial basis that will be
+#   applied to the input X. M should not be negative.
+#
+# @return Input X, after being transformed from the polynomial basis function.
+#
+# @author C.A.Kapourani \email{C.A.Kapourani@@ed.ac.uk}
+#
+# @seealso \code{\link{create_polynomial_object}}, \code{\link{rbf_basis}}
+#
+# @examples
+# out <- polynomial_basis(X = 3, M = 2)
+# #
+# out <- polynomial_basis(X = c(2, 3, 4), M = 2)
+#
+.polynomial_basis <- function(X, M = 1){
   return(X ^ M)
 }
 
 
-#' Apply the radial basis function
-#'
-#' Apply the RBF function to the input X.
-#'
-#' @param X Input data.
-#' @param mus Centers from where we should compute the distance of the data X.
-#' @param gamma Inverse width of radial basis function.
-#'
-#' @return Input X, after being transformed from the RBF.
-#'
-#' @seealso \code{\link{rbf.object}}, \code{\link{polynomial_basis}}
-#'
-#' @examples
-#' out <- rbf_basis(X = c(1,2), mus = c(1,1))
-#' #
-#' out <- rbf_basis(X = c(1,2), mus = c(1,1), gamma = 0.1)
-#'
-#' @export
-rbf_basis <- function(X, mus, gamma = 1){
+# (Internal) Apply the radial basis function
+#
+# Apply the RBF function to the input X.
+#
+# @param X Input data.
+# @param mus Centers from where we should compute the distance of the data X.
+# @param gamma Inverse width of radial basis function.
+#
+# @return Input X, after being transformed from the RBF.
+#
+# @author C.A.Kapourani \email{C.A.Kapourani@@ed.ac.uk}
+#
+# @seealso \code{\link{create_rbf_object}}, \code{\link{polynomial_basis}}
+#
+# @examples
+# out <- rbf_basis(X = c(1,2), mus = c(1,1))
+# #
+# out <- rbf_basis(X = c(1,2), mus = c(1,1), gamma = 0.1)
+#
+.rbf_basis <- function(X, mus, gamma = 1){
   return(exp( (-1) * gamma * sum((X - mus)^2)))
 }
