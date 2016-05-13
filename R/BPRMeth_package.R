9b7ad6d45132c76fbf7d9c695bb827ddc2035434
andreaskapou <kapouranis.andreas@gmail.com>
2016-05-13 13:37:51 +0100

    Add imports

--- R/BPRMeth_package.R
+++ R/BPRMeth_package.R
@@ -4,6 +4,7 @@
 #' @name BPRMeth
 #'
 #' @import GenomicRanges S4Vectors doParallel parallel foreach MASS e1071 earth randomForest
+#' @importFrom stats pnorm dbinom dnorm
 #'
 .datatable.aware=TRUE
 NULL
