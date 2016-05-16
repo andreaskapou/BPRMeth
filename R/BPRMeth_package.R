4e082589869cee397cf3948eddaecc0af2e02992
andreaskapou <kapouranis.andreas@gmail.com>
2016-05-16 11:25:56 +0100

    Add description

--- R/BPRMeth_package.R
+++ R/BPRMeth_package.R
@@ -1,9 +1,14 @@
 #' \code{BPRMeth}: Extracting higher order methylation features
 #'
+#' Higher order methylation features for clustering and prediction in epigenomic
+#' studies
 #' @docType package
 #' @name BPRMeth
 #'
-#' @import GenomicRanges S4Vectors doParallel parallel foreach MASS e1071 earth randomForest
+#' @author C.A.Kapourani \email{C.A.Kapourani@@ed.ac.uk}
+#'
+#' @import GenomicRanges S4Vectors doParallel parallel foreach MASS e1071 earth
+#'   randomForest
 #' @importFrom stats pnorm dbinom dnorm
 #'
 .datatable.aware=TRUE
